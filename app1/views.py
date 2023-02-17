be2c3e3a55ce4c6040a2f311a6074b77b21a1dbe
lohar prathmesh <loharprathmesh123@gmail.com>
2023-02-17 08:09:35 +0530

    next branch

--- app1/views.py
+++ app1/views.py
@@ -3,20 +3,25 @@ import requests
 import os
 import json
 from gnewsclient import gnewsclient
+
 # Create your views here.
 
 
 def home(request):
 
-    client = gnewsclient.NewsClient(language='english',
-                                location='india',
-                                topic='World',
-                                use_opengraph=True,
-                                max_results=20)
+    
+
+    # url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=826f05f00a2847738c60df0260a8691b')
+    # response = requests.get(url)
+    # data = response.json()
+    # articles = data['articles']
+
+    client = gnewsclient.NewsClient(language='english',location='india',topic='sports',use_opengraph=True,max_results=4)
  
     news_list = client.get_news()
-    print(news_list)
+
     context = {
+       
         'news_list':news_list,
     }
     return render(request,"base.html",context)
