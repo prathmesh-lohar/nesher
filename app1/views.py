4f9ea8eab0303c82d312fb594088ee123ab279a0
lohar prathmesh <loharprathmesh123@gmail.com>
2023-02-17 20:33:41 +0530

    tabs

--- app1/views.py
+++ app1/views.py
@@ -2,26 +2,31 @@ from django.shortcuts import render
 import requests
 import os
 import json
-from gnewsclient import gnewsclient
+from GoogleNews import GoogleNews
 
 # Create your views here.
 
-
 def home(request):
+
+
+    url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=826f05f00a2847738c60df0260a8691b')
+    response = requests.get(url)
+    data = response.json()
+    articles = data['articles'] 
+
+    googlenews = GoogleNews(lang='en', region='IN',period='7d')
+    
+    googlenews.get_news('breaking news')
 
     
 
-    # url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=826f05f00a2847738c60df0260a8691b')
-    # response = requests.get(url)
-    # data = response.json()
-    # articles = data['articles']
+    res = googlenews.results()[:25]
 
-    client = gnewsclient.NewsClient(language='english',location='india',topic='sports',use_opengraph=True,max_results=4)
- 
-    news_list = client.get_news()
+    
 
     context = {
+       'articles':articles,
+       'res':res,
        
-        'news_list':news_list,
     }
     return render(request,"base.html",context)
